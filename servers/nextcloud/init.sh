81319589822b00ac32677986e6b1796d27adf2e7
navid <navid.pdp11@gmail.com>
2022-09-28 14:22:55 +0330

    refactor and add more details

--- servers/nextcloud/init.sh
+++ servers/nextcloud/init.sh
@@ -1,8 +1,6 @@
 php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "nextcloud" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
 php console.php app:disable firstrunwizard
-php console.php app:enable mfachecker
+php console.php app:enable sciencemesh
+sudo -u www-data php console.php app:install twofactor_totp
 sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
-sed -i "9 i\      2 => 'nc2.docker'," /var/www/html/config/config.php
-sed -i "10 i\      3 => 'nc1.pondersource.net'," /var/www/html/config/config.php
-sed -i "11 i\      4 => 'nc2.pondersource.net'," /var/www/html/config/config.php
 sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
