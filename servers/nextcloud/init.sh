5b54e49b899cb351eb498f9a536dbe897491c62a
navid <navid.pdp11@gmail.com>
2022-09-29 11:27:19 +0330

    setup process finished

--- servers/nextcloud/init.sh
+++ servers/nextcloud/init.sh
@@ -1,6 +1,6 @@
-php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "nextcloud" --database-user "root" --database-pass "eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek" --database-host "$DBHOST"
+php console.php maintenance:install --admin-user "Admin" --admin-pass "!QAZ1qaz" --database "mysql" --database-name "nextcloud" --database-user "nextcloud" --database-pass "userp@ssword" --database-host "maria-db:3306"
 php console.php app:disable firstrunwizard
-php console.php app:enable sciencemesh
-sudo -u www-data php console.php app:install twofactor_totp
+php console.php app:install twofactor_totp
+php console.php app:install twofactor_webauthn
 sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
 sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
---
+++ servers/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DOCKER_BUILDKIT=0 docker compose build 
+docker compose up  
+
+
+
+# sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
+# sed -i "9 i\      2 => 'nc2.docker'," /var/www/html/config/config.php
+# sed -i "10 i\      3 => 'nc1.pondersource.net'," /var/www/html/config/config.php
+# sed -i "11 i\      4 => 'nc2.pondersource.net'," /var/www/html/config/config.php
+# sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
