5998d921f40658a617a0e31002036d5b75f81c7b
mr.vahedi <mr.vahedi@alibaba.ir>
2022-10-23 08:53:00 +0330

    add windos setup file to run dockers on windows

---
+++ servers/setup.bat
@@ -0,0 +1,22 @@
+set -e
+cd apache-php
+docker build . -t apache-php
+cd ../nextcloud
+docker build . -t sunet-nextcloud
+cd ..
+DOCKER_BUILDKIT=0 docker compose build 
+docker compose up -d 
+timeout /t 15 /nobreak
+docker exec -it sunet-nc1 ./init-master.sh
+docker exec -it sunet-nc1 chown -R www-data:www-data ../html
+
+
+docker exec -it sunet-nc2 ./init-slave.sh
+docker exec -it sunet-nc2 chown -R www-data:www-data ../html
+
+
+:: sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
+:: sed -i "9 i\      2 => 'nc2.docker'," /var/www/html/config/config.php
+:: sed -i "10 i\      3 => 'nc1.pondersource.net'," /var/www/html/config/config.php
+:: sed -i "11 i\      4 => 'nc2.pondersource.net'," /var/www/html/config/config.php
+:: sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
