97a143595531c570ff45725f428e68fec8bb0d87
Michiel de Jong <michiel@mesh.pondersource.com>
2022-10-27 10:44:23 +0000

    Fix sed commands (again)

--- servers/sunet-nextcloud/init-nc1-gss-master.sh
+++ servers/sunet-nextcloud/init-nc1-gss-master.sh
@@ -8,8 +8,8 @@ php console.php app:install user_saml
 php console.php app:install twofactor_webauthn
 php console.php app:enable globalsiteselector
 echo Editing config
-sed -i "9 i\    2 => 'sunet-nc1'," config/config.php
-sed -i "9 i\    1 => 'mesh.pondersource.org'," config/config.php
+sed -i "8 i\    2 => 'sunet-nc1'," config/config.php
+sed -i "8 i\    1 => 'mesh.pondersource.org'," config/config.php
 sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
 sed -i "3 i\  'gss.jwt.key' => '123456'," config/config.php
 sed -i "3 i\  'gss.mode' => 'master'," config/config.php
