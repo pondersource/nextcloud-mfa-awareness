f35d2bba3bf8c36c8cc4ee5a7f3f7ab6d3939640
Michiel de Jong <michiel@mesh.pondersource.com>
2022-10-28 14:24:43 +0000

    terminate quoted string

--- servers/sunet-nextcloud/init-nc1-gss-leader.sh
+++ servers/sunet-nextcloud/init-nc1-gss-leader.sh
@@ -4,13 +4,12 @@ php console.php maintenance:install --admin-user "Admin" --admin-pass "!QAZ1qaz"
 echo Enabling apps
 php console.php app:disable firstrunwizard
 php console.php app:install user_saml
-php console.php app:enable user_saml
 php console.php app:enable globalsiteselector
 echo Editing config
 sed -i "8 i\    2 => 'sunet-nc1'," config/config.php
 sed -i "8 i\    1 => 'mesh.pondersource.org'," config/config.php
 sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
-sed -i "3 i\  'gss.discovery.saml.slave.mapping' => 'location'
+sed -i "3 i\  'gss.discovery.saml.slave.mapping' => 'location'" config/config.php
 sed -i "3 i\  'gss.jwt.key' => '123456'," config/config.php
 sed -i "3 i\  'gss.master.admin' => ['admin']," config/config.php
 sed -i "3 i\  'gss.master.csp-allow' => ['*.localhost:8080', '*.localhost:8081'],"  config/config.php
