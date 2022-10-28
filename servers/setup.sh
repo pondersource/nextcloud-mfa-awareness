aa62c511e57aed35564af66965600b2a6f8a3976
Michiel de Jong <michiel@mesh.pondersource.com>
2022-10-28 08:28:38 +0000

    wait 15 seconds after ports 3306 are open

--- servers/setup.sh
+++ servers/setup.sh
@@ -16,9 +16,28 @@ docker build . -t simple-saml-php
 cd ..
 DOCKER_BUILDKIT=0 docker compose build
 docker compose up -d
-echo "Sleeping 20 seconds"
-sleep 20
-echo "Done sleeping, chowning /var/www/html/config on sunet-nc1/2"
+
+x=$(docker exec -it sunet-mdb1 ss -tulpn | grep 3306 | wc -l)
+until [ $x -ne 0 ]
+do
+  echo Waiting for sunet-mdb1 to start, this can take up to a minute ... $x
+  sleep 1
+  x=$(docker exec -it sunet-mdb1 ss -tulpn | grep 3306 | wc -l)
+done
+echo sunet-mdb1 port is open
+
+x=$(docker exec -it sunet-mdb2 ss -tulpn | grep 3306 | wc -l)
+until [ $x -ne 0 ]
+do
+  echo Waiting for sunet-mdb2 to start, this can take up to a minute ... $x
+  sleep 1
+  x=$(docker exec -it sunet-mdb2 ss -tulpn | grep 3306 | wc -l)
+done
+echo sunet-mdb2 port is open
+echo waiting another 15 seconds...
+sleep 15
+
+echo "Done waiting, chowning /var/www/html/config on sunet-nc1/2"
 docker exec sunet-nc1 chown -R www-data:www-data ./config
 docker exec sunet-nc2 chown -R www-data:www-data ./config
 
