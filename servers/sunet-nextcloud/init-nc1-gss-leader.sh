56563d7381f8f0cfe82694df3a2b9259e0fe10fb
navid <navid.pdp11@gmail.com>
2022-11-03 17:53:28 +0330

    enable user_saml instead of installing
    Signed-off-by: navid <navid.pdp11@gmail.com>

--- servers/sunet-nextcloud/init-nc1-gss-leader.sh
+++ servers/sunet-nextcloud/init-nc1-gss-leader.sh
@@ -3,7 +3,7 @@ echo Installing Nextcloud
 php console.php maintenance:install --admin-user "Admin" --admin-pass "!QAZ1qaz" --database "mysql" --database-name "nextcloud" --database-user "nextcloud" --database-pass "userp@ssword" --database-host "sunet-mdb1"
 echo Enabling apps
 php console.php app:disable firstrunwizard
-php console.php app:install user_saml
+php console.php app:enable user_saml
 php console.php app:enable globalsiteselector
 echo Editing config
 sed -i "8 i\    2 => 'sunet-nc1'," config/config.php
