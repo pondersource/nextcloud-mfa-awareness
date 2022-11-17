82e871e267cc0a0f6f2ca3e24ce1e93ffa9aaef2
Michiel de Jong <michiel@unhosted.org>
2022-11-17 11:56:10 +0000

    Assuming that you have mounted the user_saml app from the host

--- servers/sunet-nextcloud/init-nc2-local-saml.sh
+++ servers/sunet-nextcloud/init-nc2-local-saml.sh
@@ -4,8 +4,8 @@ set -e
 echo Installing Nextcloud
 php console.php maintenance:install --admin-user "Admin" --admin-pass "!QAZ1qaz" --database "mysql" --database-name "nextcloud" --database-user "nextcloud" --database-pass "userp@ssword" --database-host "sunet-mdb2"
 echo Enabling apps
-php console.php app:disable firstrunwizard
-php console.php app:install user_saml
+echo Assuming that you have mounted the user_saml app from the host
+php console.php app:enable user_saml
 php console.php app:enable mfachecker
 echo Editing config
 sed -i "8 i\    2 => 'sunet-nc2'," config/config.php
