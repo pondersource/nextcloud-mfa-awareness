24aa480a3ad09bb9651cb863dcd29bc8c5043524
Michiel de Jong <michiel@unhosted.org>
2022-11-17 12:14:36 +0000

    rerun build in command

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -2,4 +2,7 @@
 set -e
 
 cd servers
+docker compose -f docker-compose-saml.yaml build base-app
+docker compose -f docker-compose-saml.yaml build
+docker compose -f docker-compose-saml.yaml pull
 ./setup-saml.sh
