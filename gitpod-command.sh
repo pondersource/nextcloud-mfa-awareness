f28ad2b0f0ae035234f73213c14961f85379fd5d
Michiel de Jong <michiel@unhosted.org>
2022-11-17 12:23:59 +0000

    build js

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -6,3 +6,4 @@ docker compose -f docker-compose-saml.yaml build base-app
 docker compose -f docker-compose-saml.yaml build
 docker compose -f docker-compose-saml.yaml pull
 ./setup-saml.sh
+make watch-js
--- gitpod-init.sh
+++ gitpod-init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-
+git submodule update --init
+cd server
+npm install
+make build-js
 cd servers
 docker compose -f docker-compose-saml.yaml build base-app
 docker compose -f docker-compose-saml.yaml build
