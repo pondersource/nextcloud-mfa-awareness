f3ab596a1008246b93f7f6e341fe50dfa758d35f
Michiel de Jong <michiel@unhosted.org>
2022-11-18 11:16:07 +0000

    don't build in command

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -2,8 +2,5 @@
 set -e
 
 cd servers
-docker compose -f docker-compose-saml.yaml build base-app
-docker compose -f docker-compose-saml.yaml build
-docker compose -f docker-compose-saml.yaml pull
 ./setup-saml.sh
 make watch-js
