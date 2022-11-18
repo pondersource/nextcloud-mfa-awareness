6df219ccf7d1a834203b8cf8fe6aea25ceeea01c
Michiel de Jong <michiel@unhosted.org>
2022-11-18 11:19:35 +0000

    fix gitpod-init.sh

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -4,7 +4,7 @@ git submodule update --init
 cd server
 npm install
 make build-js
-cd servers
+cd ../servers
 docker compose -f docker-compose-saml.yaml build base-app
 docker compose -f docker-compose-saml.yaml build
 docker compose -f docker-compose-saml.yaml pull
