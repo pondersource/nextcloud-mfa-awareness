377e0f58e8a1d069856a4d337ff98021b651b3ee
Michiel de Jong <michiel@unhosted.org>
2023-03-10 15:49:14 +0000

    copy init files to host mount and chown all of /var/www/html to  www-data

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -4,6 +4,7 @@ git submodule update --init
 cd server
 npm install
 make build-js
+cp ../servers/sunet-nextcloud/init-* .
 cd ../servers
 docker compose -f docker-compose-saml.yaml build base-app
 docker compose -f docker-compose-saml.yaml build sunet-nc2
