b4abc67c55e2b0539eda97d1628e5663576e9dd8
Michiel de Jong <michiel@unhosted.org>
2023-03-15 12:04:19 +0000

    Fix #47

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -6,8 +6,9 @@ npm install
 make build-js
 cp ../servers/sunet-nextcloud/init-* .
 cd ../servers
-docker compose -f docker-compose-saml.yaml build base-app
-docker compose -f docker-compose-saml.yaml build sunet-nc2
-docker compose -f docker-compose-saml.yaml build sunet-ssp
-docker compose -f docker-compose-saml.yaml build
-docker compose -f docker-compose-saml.yaml pull
+# FIXME: the following commands don't seem to work on GitPod:
+# docker compose -f docker-compose-saml.yaml build base-app
+# docker compose -f docker-compose-saml.yaml build sunet-nc2
+# docker compose -f docker-compose-saml.yaml build sunet-ssp
+# docker compose -f docker-compose-saml.yaml build
+# docker compose -f docker-compose-saml.yaml pull
